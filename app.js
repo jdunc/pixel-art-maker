b33aa5f7fb6784fcdf74c08cc6557874e82c0b2d
Jordan Duncan <j.duncan@rochester.edu>
2016-10-28 16:31:32 -0500

    finished with pixels, random colors, displaying current color, dynamic resizing of pixel grid

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+// these first 10 codes fill the container with pixels, it was intended to be able to fill a dynamically sized
+var newWidth = $(window).width();
+$('.container').css("width", newWidth);
+var width = parseInt($('.container').css("width"));
+console.log(width + 'px');
+var height = parseInt($('.container').css("height"));
+var totalPixels = Math.ceil(height*width)/150;
+console.log(height);
+console.log(totalPixels);
+for (var i = 0; i < totalPixels; i++) {
+  console.log(i);
+var tempdiv = $( "<div id='pixel" + i + "' class='pixel'></div>" );
+  $('.container').append(tempdiv);
+  }
+  // $('.container').css("width") = width+'px';
+  // $('.container').css("height") = $('.container').css("height");
+  // $('.container').resizable('destroy');
+
+// here I create the randomized colors on click of the randomize text
+$('#randomize').click(randomizeColors);
+
 var color ='';
 var mouseCheck ='';
+$(document).mousedown(function(){
+  mouseCheck='down';
+});
+
+$(document).mouseup(function(){
+  mouseCheck='up';
+});
 
 $('.pal').click(function(event){
   var thisdiv = '#' + event.target.id;
@@ -8,6 +36,8 @@ $('.pal').click(function(event){
   // var te = event.target;
   //  console.log($(event.target).attr('class'));
    color = $(thisdiv).css('backgroundColor');
+   $('#randomize').css('background-color', color)
+
   // console.log($(this).css('backgroundColor'));
 })
 
@@ -21,13 +51,7 @@ $('.pixel').mouseenter(function(){
   }
 });
 
-$(document).mousedown(function(){
-  mouseCheck='down';
-});
 
-$(document).mouseup(function(){
-  mouseCheck='up';
-});
 // console.log($('.pc').length);
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -45,5 +69,4 @@ function randomizeColors(){
     $(thisdiv).css( "background-color", tempColor)
   }
 }
-
 randomizeColors();
